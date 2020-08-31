dca969d3d2f1250a66f6b997f6c5483c25b87680
SkSumit <kolsum24@gmail.com>
2020-08-31 16:09:39 +0530

    setup prod build

--- source/app.js
+++ source/app.js
@@ -14,13 +14,6 @@ import { setTextFilter } from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
 
 const store = configureStore();
-// console.log(store.getState());
-// store.dispatch(
-//   addExpense({
-//     description: "Gas Bill",
-//     amount: 1700,
-//   })
-// );
 
 store.dispatch(
   addExpense({
@@ -29,21 +22,7 @@ store.dispatch(
     date: 1596263400000,
   })
 );
-// store.dispatch(
-//   addExpense({
-//     description: "Rent Bill",
-//     amount: 12000,
-//     date: 30082020,
-//   })
-// );
 
-// store.dispatch(setTextFilter("water"));
-
-// const state = store.getState();
-// const vExp = getVisibleExpenses(state.expenses, state.filter);
-// console.log(vExp);
-
-// console.log(store.getState());
 console.log("Running from script");
 
 const jsx = (
